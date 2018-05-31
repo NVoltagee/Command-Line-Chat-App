28d4cea4243108f88e11f112bad3f3582b2c06e9
Norman Trujillo <natrujillo9@gmail.com>
2018-05-31 11:42:35 -0400

    Created rooms, user can subscribe to a room and also send messages.

--- client.js
+++ client.js
@@ -1,11 +1,19 @@
 const prompt = require('prompt');
 const util = require('util');
 const axios = require('axios');
-const { ChatManager, TokenProvider } = require('@pusher/chatkit');
-const { JSDOM } = require('jsdom');
+const {
+    ChatManager,
+    TokenProvider
+} = require('@pusher/chatkit');
+const {
+    JSDOM
+} = require('jsdom');
+const readline = require('readline');
 
 const makeChatkitNodeCompatible = () => {
-    const { window } = new JSDOM();
+    const {
+        window
+    } = new JSDOM();
     global.window = window;
     global.navigator = {};
 };
@@ -46,11 +54,47 @@ const main = async () => {
             tokenProvider: new TokenProvider({
                 url: 'http://localhost:3001/authenticate'
             })
-        });
+        })
 
         const currentUser = await chatManager.connect()
-        console.log(currentUser)
 
+        const joinableRooms = await currentUser.getJoinableRooms()
+        const availableRooms = [...currentUser.rooms, ...joinableRooms]
+        availableRooms.forEach((room, index) => {
+            console.log(`${index} - ${room.name}`)
+        })
+
+        const roomStructure = [{
+            description: 'Select a room',
+            name: 'chosenRoom',
+            required: true
+        }]
+        const {
+            chosenRoom
+        } = await get(roomStructure)
+        const room = availableRooms[chosenRoom]
+
+        await currentUser.subscribeToRoom({
+            roomId: room.id,
+            hooks: {
+                onNewMessage: message => {
+                    if (message.senderId !== username)
+                        console.log(`${message.senderId} - ${message.text}`)
+                }
+            },
+            messageLimit: 0
+        })
+
+        const input = readline.createInterface({
+            input: process.stdin
+        })
+
+        input.on('line', async text => {
+            await currentUser.sendMessage({
+                roomId: room.id,
+                text
+            })
+        })
     } catch (error) {
         console.error(error)
         process.exit(1)
